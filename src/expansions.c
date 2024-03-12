7a121aa65481951908670d592e7169e982afa13e
Roger de Moura <rde-mour@student.42sp.org.br>
2024-03-12 14:25:01 -0300

    feat: add expansions init and expand environment variables

---
+++ src/expansions.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expansions.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 09:08:52 by rde-mour          #+#    #+#             */
+/*   Updated: 2024/03/12 14:20:51 by rde-mour         ###   ########.org.br   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_linkedlist.h"
+#include "ft_string.h"
+#include "expansions.h"
+#include "get_env.h"
+#include "tokens.h"
+#include "ft_printf_bonus.h"
+
+static char	*expand(char	*begin, char *var, char *end)
+{
+	char	*tmp;
+	char	*string;
+
+	string = ft_strjoin(begin, var);
+	free(begin);
+	tmp = string;
+	string = ft_strjoin(tmp, end);
+	free(end);
+	free(tmp);
+	return (string);
+}
+
+static char	*var_to_string(t_var *var)
+{
+	char	**splitted;
+	char	*tmp;
+	char	*tmp2;
+
+	if (!var)
+		return (ft_strdup(""));
+	tmp = NULL;
+	splitted = var->values;
+	while (*splitted)
+	{
+		tmp2 = NULL;
+		if (!tmp)
+			tmp = ft_strdup(*(splitted)++);
+		else
+			ft_sprintf(&tmp2, "%s:%s", tmp, *(splitted)++);
+		if (tmp2)
+		{
+			free(tmp);
+			tmp = ft_strdup(tmp2);
+			free(tmp2);
+		}
+	}
+	return (tmp);
+}
+
+static char	*check_expansion(t_env *env, char *word)
+{
+	int		start;
+	int		end;
+	char	*tmp;
+	char	*value;
+
+	if (!env)
+		return (ft_strdup(""));
+	start = 0;
+	while (!ft_strchr("$\0", *(word + start)))
+		start++;
+	end = ++start;
+	while (!ft_strchr(" $\t\"\'\0", *(word + end)))
+		end++;
+	tmp = ft_substr(word, start, end - start);
+	value = var_to_string(envget(&(env->envp), tmp));
+	if (!(*value))
+	{
+		free(value);
+		value = var_to_string(envget(&(env->exports), tmp));
+	}
+	free(tmp);
+	tmp = expand(ft_substr(word, 0, start - 1), value,
+			ft_substr(word, end, ft_strlen(&(*(word + end)))));
+	free(value);
+	return (tmp);
+}
+
+void	expansions(t_env *env, t_list **tokens)
+{
+	char		*string;
+	t_list		*tmp;
+	t_content	*content;
+
+	if (!env || !tokens)
+		return ;
+	tmp = *tokens;
+	while (tmp)
+	{
+		content = (t_content *) tmp->content;
+		while (content->token != QUOTE && ft_strchr(content->string, '$'))
+		{
+			string = check_expansion(env, content->string);
+			free(content->string);
+			content->string = string;
+		}
+		tmp = tmp->next;
+	}
+}
--- src/main.c
+++ src/main.c
@@ -6,37 +6,40 @@
 /*   By: rde-mour <rde-mour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 19:09:53 by rde-mour          #+#    #+#             */
-/*   Updated: 2024/03/10 15:26:00 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/03/12 14:20:36 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include "ft_string.h"
 #include "prompt.h"
 #include "types.h"
 #include "tokens.h"
 #include "ft_linkedlist.h"
+#include "expansions.h"
+#include "get_env.h"
 
-static void	tokens(char **splitted)
+static void	tokens(t_env *env, char **splitted)
 {
 	t_list	*tokens;
 	t_list	*tmp;
 
 	tokens = tokenizer(splitted);
+	expansions(env, &tokens);
 	while (tokens)
 	{
 		tmp = tokens;
 		tokens = tokens->next;
 		printf("%d %s\n", ((t_content *) tmp->content)->token,
 			((t_content *) tmp->content)->string);
+		free(((t_content *) tmp->content)->string);
 		free(tmp->content);
 		free(tmp);
 	}
 }
 
-static void	prompt(void)
+static void	prompt(t_env *env)
 {
 	char	*input;
 	char	**splitted;
@@ -49,16 +52,26 @@ static void	prompt(void)
 		else if (*input)
 			add_history(input);
 		splitted = format_input(input);
-		tokens(splitted);
+		tokens(env, splitted);
 		if (splitted)
-			ft_memclear((void **) splitted, free);
+			free(splitted);
 		free(input);
 	}
 	rl_clear_history();
 }
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
-	prompt();
+	t_env	env;
+
+	if (argc > 1 || !argv)
+		return (EXIT_FAILURE);
+	env.envp = arrnew();
+	env.exports = arrnew();
+	envadd(&env.envp, "VAR", "Roger de Moura");
+	envnew(&env.envp, envp);
+	prompt(&env);
+	envclear(&env.envp);
+	envclear(&env.exports);
 	return (EXIT_SUCCESS);
 }
