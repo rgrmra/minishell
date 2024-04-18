cefec602aacb466adb99817f70f0e2a890db999d
Roger de Moura <rde-mour@student.42sp.org.br>
2024-04-17 22:02:57 -0300

    feat: add command and pipe execution

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 20:00:16 by rde-mour          #+#    #+#             */
-/*   Updated: 2024/04/16 21:34:17 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/04/17 22:02:02 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,29 @@
 #include "ft_string.h"
 #include <fcntl.h>
 
-static void	execute_command(t_env *env, t_ast **ast, int *lfd)
+static void	execute(t_env *env, t_ast **ast, int *fds);
+
+static void	open_stdout(int **fds)
+{
+	dup2(*fds[1], STDOUT_FILENO);
+	close(*fds[0]);
+}
+
+//static void	open_stdin(int **fds)
+//{
+//	dup2(*fds[0], STDIN_FILENO);
+//	close(*fds[0]);
+//	close(*fds[1]);
+//}
+
+static void	closeall()
+{
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+}
+
+static void	execute_command(t_env *env, t_ast **ast, int *fds)
 {
 	pid_t	pid;
 	char	**cmd;
@@ -43,67 +65,67 @@ static void	execute_command(t_env *env, t_ast **ast, int *lfd)
 	pid = fork();
 	if (pid == 0)
 	{
+		if (fds)
+			open_stdout(&fds);
 		rl_clear_history();
-		if (lfd)
-		{
-			lfd[2] = open("file", O_TRUNC | O_CREAT | O_RDWR, 0644);
-			dup2(lfd[0], STDOUT_FILENO);
-			close(lfd[0]);
-			close(lfd[1]);
-			dup2(lfd[2], STDIN_FILENO);
-			close(lfd[2]);
-		}
 		if (*cmd && (ft_strchr("./", **cmd) || access(*cmd, F_OK | X_OK) == 0)
-			&& execve(*cmd, cmd, env->environ) < 0)
+				&& execve(*cmd, cmd, env->environ) < 0)
 			(printf("failed\n"), exit(EXIT_FAILURE));
 		printf("command not found!\n");
 		ft_memclear((void **) cmd, &free);
-		exit(EXIT_FAILURE);
+		closeall();
+		exit(EXIT_SUCCESS);
 	}
+	closeall();
 	ft_memclear((void **) cmd, &free);
 	waitpid(pid, NULL, WUNTRACED);
 }
 
-static void	execute_open(t_env *env, t_ast **ast)
+static void	execute_pipe(t_env *env, t_ast **ast)
 {
 	pid_t	pid;
-	t_ast	*tmp;
-	int	fd[3];
+	int		fds[4];
 
-	tmp = *ast;
-	var_expansions(env, tmp->left->content);
-	remove_quotes_aux(tmp->left->content->literal);
+	pipe(fds);
 	pid = fork();
-	pipe(fd);
 	if (pid == 0)
 	{
-		rl_clear_history();
-		fd[2] = open(tmp->left->content->literal, O_RDONLY, 0644);
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		dup2(fd[2], STDIN_FILENO);
-		close(fd[2]);
-		execute_command(env, &((*ast)->right), fd);
-		exit(EXIT_FAILURE);
+		dup2(fds[1], STDOUT_FILENO);
+		close(fds[0]);
+		close(fds[1]);
+		execute(env, &((*ast)->left), fds);
+		closeall();
+		exit(EXIT_SUCCESS);
 	}
-	waitpid(pid, NULL, WUNTRACED);
+	pid = fork();
+	if (pid == 0)
+	{
+		dup2(fds[0], STDIN_FILENO);
+		close(fds[0]);
+		close(fds[1]);
+		execute(env, &((*ast)->right), fds);
+		closeall();
+		exit(EXIT_SUCCESS);
+	}
+	close(fds[0]);
+	close(fds[1]);
+	waitpid(pid, NULL, WCONTINUED);
 }
 
-static void	execute(t_env *env, t_ast **ast)
+static void	execute(t_env *env, t_ast **ast, int *fds)
 {
 	if (!ast || !(*ast))
 		return ;
 	if ((*ast)->content->type & COMMAND)
-		execute_command(env, ast, NULL);
+		execute_command(env, ast, fds);
 	else if ((*ast)->content->type & (LESS | DLESS))
-		execute_open(env, ast);
+		printf("Open redirect infile %s\n", (*ast)->content->literal);
 	else if ((*ast)->content->type & GREATER)
-		printf("Open output file %s\n", (*ast)->content->literal);
+		printf("Open redirect output file %s\n", (*ast)->content->literal);
 	else if ((*ast)->content->type & DGREATER)
 		printf("Open append output file %s\n", (*ast)->content->literal);
 	else if ((*ast)->content->type & VBAR)
-		printf("Open pipeline %s\n", (*ast)->content->literal);
+		execute_pipe(env, ast);
 	else if ((*ast)->content->type & AND)
 		printf("Check AND operator %s\n", (*ast)->content->literal);
 	else if ((*ast)->content->type & OR)
@@ -123,7 +145,7 @@ static void	tokens(t_env *env, char **splitted)
 	parser(&tokens);
 	(void)env;
 	ast = ast_new(&tokens);
-	execute(env, &ast);
+	execute(env, &ast, NULL);
 	ast_clear(&ast);
 }
 
